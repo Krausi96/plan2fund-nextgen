72c624967bcb761f50d94113af86bccd25a12272
Krausi96 <133792501+Krausi96@users.noreply.github.com>
2025-08-27 12:52:09 +0200

    fix: removed duplicate AppShell wrapper from landing page to prevent double header/footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
-﻿import AppShell from "@/components/layout/AppShell";
-import Hero from "@/components/landing/Hero";
+﻿import Hero from "@/components/landing/Hero";
 import Quote from "@/components/landing/Quote";
 import UseCases from "@/components/landing/UseCases";
 import PlanTypes from "@/components/landing/PlanTypes";
@@ -7,14 +6,12 @@ import Included from "@/components/landing/Included";
 
 export default function Home() {
   return (
-    <AppShell>
-      <div className="flex flex-col space-y-12">
-        <Hero />
-        <Quote />
-        <UseCases />
-        <PlanTypes />
-        <Included />
-      </div>
-    </AppShell>
+    <div className="flex flex-col space-y-12">
+      <Hero />
+      <Quote />
+      <UseCases />
+      <PlanTypes />
+      <Included />
+    </div>
   );
 }
