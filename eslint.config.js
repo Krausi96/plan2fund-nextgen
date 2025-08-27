be854ec48ca585a703c1251efdfe2d051090a105
Krausi96 <133792501+Krausi96@users.noreply.github.com>
2025-08-27 11:15:27 +0200

    fix: enforce Next.js 14 structure (eslint, postcss, package.json cleanup)

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,20 @@
-﻿import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+﻿import nextPlugin from "@next/eslint-plugin-next";
+import globals from "globals";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    ignores: ["**/node_modules/**", "**/.next/**", "**/dist/**"],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      "react/jsx-key": "warn",
+    },
   },
-])
+];
