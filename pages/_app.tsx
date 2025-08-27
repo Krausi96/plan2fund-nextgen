29e1de92ea6d67f6cb26cf16c0cd01aa4fdc17d5
Krausi96 <133792501+Krausi96@users.noreply.github.com>
2025-08-27 10:03:12 +0200

    fix: correct Header, Footer, Breadcrumbs imports in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,19 @@
 ﻿import type { AppProps } from "next/app"
 import "@/styles/globals.css"
-import { Header } from "@/components/layout/Header"
-import { Footer } from "@/components/layout/Footer"
-import { Breadcrumbs } from "@/components/layout/Breadcrumbs"
+
+import Header from "@/components/layout/Header"
+import Footer from "@/components/layout/Footer"
+import Breadcrumbs from "@/components/layout/Breadcrumbs"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 container mx-auto py-6">
-        <Breadcrumbs />
+      <Breadcrumbs />
+      <main className="flex-1">
         <Component {...pageProps} />
       </main>
       <Footer />
     </div>
   )
 }
-
