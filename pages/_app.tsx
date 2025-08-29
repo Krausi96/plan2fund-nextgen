2586ddab85d4d4ec1c294d9c8aea24aa43628012
Krausi96 <133792501+Krausi96@users.noreply.github.com>
2025-08-29 13:38:26 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-﻿import "../styles/globals.css"
-import type { AppProps } from "next/app"
-import AppShell from "@/components/layout/AppShell"
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import AppShell from "@/components/layout/AppShell";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AppShell>
-      <Component {...pageProps} />
-    </AppShell>
-  )
+    <div className={inter.variable} style={{ fontFamily: "var(--font-inter), system-ui, sans-serif" }}>
+      <AppShell>
+        <Component {...pageProps} />
+      </AppShell>
+    </div>
+  );
 }
-
-
