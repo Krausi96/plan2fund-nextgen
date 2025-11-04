3efa5b6a069df2719029b52cc8b8a49d163fbaef
Krausi96 <133792501+Krausi96@users.noreply.github.com>
2025-11-04 16:06:11 +0100

    Replace broken SmartWizard with simple, reliable SimpleWizard - 7 clear questions with all answer options

--- pages/reco.tsx
+++ pages/reco.tsx
@@ -1,4 +1,4 @@
-﻿import SmartWizard from "@/features/reco/components/wizard/SmartWizard";
+﻿import SimpleWizard from "@/features/reco/components/wizard/SimpleWizard";
 import { useEffect } from "react";
 import analytics from "@/shared/lib/analytics";
 
@@ -7,25 +7,10 @@ function RecoPage() {
     analytics.trackPageView('/reco', 'Recommendations');
     analytics.trackUserAction('reco_started', {});
   }, []);
-  // Lightweight render timing to investigate slow loads
-  if (typeof window !== 'undefined') {
-    performance.mark('reco-render-start');
-    requestAnimationFrame(() => {
-      performance.mark('reco-render-end');
-      performance.measure('reco-render', 'reco-render-start', 'reco-render-end');
-      const measures = performance.getEntriesByName('reco-render');
-      if (measures && measures[0]) {
-        console.log(`[Reco] Render time: ${Math.round(measures[0].duration)}ms`);
-        performance.clearMeasures('reco-render');
-        performance.clearMarks('reco-render-start');
-        performance.clearMarks('reco-render-end');
-      }
-    });
-  }
 
   return (
     <div className="max-w-5xl mx-auto p-6">
-      <SmartWizard />
+      <SimpleWizard />
     </div>
   );
 }
